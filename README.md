ef23dc82a5e01e54802cf499adcdd3b39e139ff6
Marlon Card <marloncard@gmail.com>
2019-03-23 08:56:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# SIMPLE FTP SERVER
+
+This is a ftp server using the python library: pyftplib. I'll only be using with my raspberry pi for the most part.
+Code is initially based on the [tutorial](https://pyftpdlib.readthedocs.io/en/latest/tutorial.html), which I plan on expanding on a bit.
+
+## 1. Install Python
+
+Versions 2.6 to 3.5 are compatible with pyftplib
+
+```
+$ sudo apt-get install python3=3.5.1*
+```
+ 
+## 2. Install pyftplib
+
+
+```
+$ pip install pyftplib
+```
---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+def main():
+    # Instantiate a dummy authorizer for managing 'virtual' users
+    authorizer = DummyAuthorizer()
+
+    """
+    Define new user having full r/w permissions and a read-only 
+    anonymous user
+    perm (Permissions):
+    *Read*
+    'e' = change directory(CWD, CDUP commands)
+    'l' = list files (LIST, NLST, STAT, MLSD, MLST, SIZE commands)
+    'r' = retrieve file from the server (RETR command)
+    *Write*
+    'a' = append data to an existing file (APPE command)
+    'd' = delete file or directory (DELE, RMD, commands)
+    'f' = rename file or directory (RNFR, RNTO commands)
+    'm' = create directory (MKD command)
+    'w' = store a file to the server (STOR, STOU commands)
+    'M' = change file mode / permission (SITE CHMOD command)
+    'T' = change file modification time (SITE MFMT command)
+    """
+    authorizer.add_user('user', '12345', '.', perm='elradfmwMT')
+    authorizer.add_anonymous(os.getcwd())
+
+    # Instantiate FTP handler class
+    handler = FTPHandler
+    handler.authorizer = authorizer
+
+    # Define a customized banner (string returned when client connects)
+    handler.banner = "pyftpdlib based ftpd ready."
+
+    
